6ae037333675444f5a55d633b1342944604e1924
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-04 12:04:47 -0700

    update id's in project to reflect celeb quiz nature
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form#celeb").submit(function(event) {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
+    const flavor = $("input:radio[name=flavor]:checked").val();
 
     if (age) {
       let quote = (100 - age) * 3;
